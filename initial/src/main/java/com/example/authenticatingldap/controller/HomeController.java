e08bce8d9dcdb9211574a0d0f926d50d856036ae
nishatoma <nishatoma@gmail.com>
2021-12-18 01:56:15 -0500

    First commit

--- initial/src/main/java/com/example/authenticatingldap/controller/HomeController.java
+++ initial/src/main/java/com/example/authenticatingldap/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.authenticatingldap;
+package com.example.authenticatingldap.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
